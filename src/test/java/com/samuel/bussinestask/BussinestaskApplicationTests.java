b208a2d04bef72cced2d38d73418614e8c8d752a
Samuel Cardona <scardona0297@gmail.com>
2026-01-21 09:46:05 -0500

    fase inicial

---
+++ src/test/java/com/samuel/bussinestask/BussinestaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.bussinestask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BussinestaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
